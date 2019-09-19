4803121b057d7899929ef812dc61e611a1067786
BuildTools <unconfigured@null.spigotmc.org>
2019-09-19 02:36:57 -0400

    added negative check

--- src/me/popcwrn/unbreakablepatch/Main.java
+++ src/me/popcwrn/unbreakablepatch/Main.java
@@ -16,7 +16,7 @@ public class Main extends JavaPlugin implements Listener {
     @EventHandler
     public void OnInventoryOpen(InventoryOpenEvent event) {
         for (ItemStack itemStack : event.getPlayer().getInventory().getContents()) {
-            if (itemStack != null && itemStack.getItemMeta().isUnbreakable() || itemStack.getDurability() > itemStack.getType().getMaxDurability()) {
+            if (itemStack != null && itemStack.getItemMeta().isUnbreakable() || itemStack.getDurability() > itemStack.getType().getMaxDurability()|| itemStack.getDurability() < 0) {
                 itemStack.setDurability(itemStack.getType().getMaxDurability());
                 itemStack.getItemMeta().setUnbreakable(false);
             }
